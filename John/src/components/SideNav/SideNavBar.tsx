a1780d272f1f29f0019ebbab8f20533b5c255c8e
Jethro <help@crowdsourcesolutions.ph>
2025-08-19 16:32:34 +0800

    fixed header position and side navigation

--- John/src/components/SideNav/SideNavBar.tsx
+++ John/src/components/SideNav/SideNavBar.tsx
@@ -12,11 +12,12 @@ import {
 } from '@tabler/icons-react';
 import { Group } from '@mantine/core';
 import classes from './SideNavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 
 const SideBarLinks = [
     
-    {link: '', label: 'Invoices', icon: IconFileInvoice},
+    {link: '/invoice', label: 'Invoices', icon: IconFileInvoice},
     {link: '', label: 'Collection Receipts', icon: IconReceiptDollar},
     {link: '', label: ' Petty Cash', icon: IconCash},
     {link: '', label: 'Bills', icon: IconInvoice},
@@ -27,36 +28,33 @@ const SideBarLinks = [
 export function SideNavBar() {
     const [active, setActive] = useState('Dashboard');
     const links = SideBarLinks.map((item) => (
-            <a
+            <NavLink
+                 to={item.link}
       className={classes.link}
-      data-active={item.label === active || undefined}
-      href={item.link}
       key={item.label}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(item.label);
-      }}
+      onClick={() => setActive(item.label)}
+
     >
       <item.icon className={classes.linkIcon} stroke={1.5} />
       <span>{item.label}</span>
-    </a>
+    </NavLink>
   ));
 
     return (
         <nav className={classes.navbar}>
       <div className={classes.navbarMain}>
         <Group className={classes.header} >
-              <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+              <NavLink to="/" className={classes.link} onClick={() => setActive("Dashboard")}>
           <IconHome className={classes.linkIcon} stroke={1.5} />
           <span>Dashboard</span>
-        </a>
+        </NavLink>
         </Group>
         {links}
         <Group className={classes.documentation} justify="space-between">
-            <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+            <NavLink to="/Documentation" className={classes.link} onClick={(event) => event.preventDefault()}>
             <IconClipboard className={classes.linkIcon} stroke={1.5} />
           <span>Documentation</span>
-        </a>
+        </NavLink>
         </Group>
       </div>
 
