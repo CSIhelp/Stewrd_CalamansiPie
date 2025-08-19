5d89eb44ef8a9b37dfefd5dcb38f1777275aa0f4
Jethro <help@crowdsourcesolutions.ph>
2025-08-19 16:29:23 +0800

    Static Invoice Page

--- John/src/pages/Invoice/Invoice.tsx
+++ John/src/pages/Invoice/Invoice.tsx
@@ -1,16 +1,56 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-
-import { Card, Group } from "@mantine/core";
+import { Card, Group, Container } from "@mantine/core";
 import { SideNavBar } from "../../components/SideNav/SideNavBar";
-import "./Dashboard.css";
+import "./Invoice.css";
 import NewItemCard from "../../components/NewItemCard/NewItemCard";
 import ReportItemCard from "../../components/ReportItemCard/ReportItemCard";
+
+
 function Invoice () {
-    return (
-<div>
+
+    return(
     
+        <>
+      
+<div>
+<Header title="Invoices"/>
+<div className="InvoiceContainer">
+<SideNavBar />
+<Container className="InvoiceCardContainer">
+<Card withBorder radius="md" p="lg" className="InvoiceCard">
+<Group className="InvoiceTitleCard">
+<h1>Invoices</h1>
+</Group>
+
+{/* Invoice Item card 1 Reusable */}
+<NewItemCard 
+    title="Invoices Upload"
+    description="Upload your invoices to the system for processing."
+    buttonText="View Portal"
+    buttonLink="https://example.com/invoices-upload"
+    />
+ 
+</Card>
+<Card withBorder radius="md" p="lg" className="InvoiceCard">
+<Group className="InvoiceTitleCard">
+<h1> Reports </h1>
+</Group>
+
+{/* Invoice Item card 1 Reusable */}
+<ReportItemCard 
+    title="Invoice Google Sheets"
+    description="Access pre-configured templates for data entry and reporting."
+    buttonText="View Report"
+    buttonLink="https://example.com/collection-receipts"    
+/>
+ 
+</Card>
+   </Container>
 </div>
+</div>
+  
+    </>
     )
 }
 export default Invoice;
