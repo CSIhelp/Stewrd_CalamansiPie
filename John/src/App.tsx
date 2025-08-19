641d5549ea45f38a68535b99780746c6f1d170eb
Jethro <help@crowdsourcesolutions.ph>
2025-08-19 15:55:57 +0800

    added invoice page ( empty )

--- John/src/App.tsx
+++ John/src/App.tsx
@@ -1,15 +1,19 @@
 
 import './App.css'
 import Dashboard from './pages/Dashboard/Dashboard'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Invoice from './pages/Invoice/Invoice'
 function App() {
 
 
   return (
     <>
-      <div>
-      <Dashboard />
-      </div>
- 
+    <BrowserRouter>
+    <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/invoice" element={<Invoice />} />
+        </Routes>
+ </BrowserRouter>
     </>
   )
 }
---
+++ John/src/pages/Invoice/Invoice.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+import { Card, Group } from "@mantine/core";
+import { SideNavBar } from "../../components/SideNav/SideNavBar";
+import "./Dashboard.css";
+import NewItemCard from "../../components/NewItemCard/NewItemCard";
+import ReportItemCard from "../../components/ReportItemCard/ReportItemCard";
+function Invoice () {
+    return (
+<div>
+    
+</div>
+    )
+}
+export default Invoice;
