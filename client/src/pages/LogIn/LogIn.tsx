e4a1fa2e44ef86539e68737ef4a04b0a3475c1d4
Luan <franciscodgail@gmail.com>
2025-09-02 13:38:23 +0800

    Integration to fronend deployment of back end deployment

--- client/src/pages/LogIn/LogIn.tsx
+++ client/src/pages/LogIn/LogIn.tsx
@@ -18,7 +18,7 @@ function LogIn() {
 
 const handleLogIn = async () => {
 try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("http://https://john-srlj.onrender.com/api/auth/login", {
         ClientId: clientId,
         Password: password,
       })
